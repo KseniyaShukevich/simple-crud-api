ed6190f86fd571aeb5c3cce2cd7bbabaed2b24ee
kseniya shukevich <kseniya.shukevich@gmail.com>
2023-01-15 04:05:47 +0100

    feat: Add url parser

---
+++ src/framework/middlewares/urlParser.ts
@@ -0,0 +1,14 @@
+import RequestType from '../RequestType';
+
+const urlParser = (req: RequestType) => {
+  const parsedUrl = req.url?.split('/');
+  const idIndex = 2;
+
+  if (parsedUrl) {
+    const id = parsedUrl[idIndex];
+
+    req.id = id;
+  }
+};
+
+export default urlParser;
